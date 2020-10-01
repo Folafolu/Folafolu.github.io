44a0ae7039f893dcf9b80e47ee5e64707ffa4c11
Folafolu <71724274+Folafolu@users.noreply.github.com>
2020-10-01 11:46:35 -0600

    Update sketch.js

--- 2d Project Assignment/sketch.js
+++ 2d Project Assignment/sketch.js
@@ -14,7 +14,6 @@ let cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //grid = generateEmptyGrid(4);
   cellWidth = width/GRIDSIZE;
   cellHeight = height/GRIDSIZE;
 }
@@ -25,22 +24,12 @@ function draw() {
   boxMove();
 }
 
-// function keyPressed(){
-//   if(key === " "){
-//     grid = generateRandomGrid(10);
-//   }
-// }
 
 function displayGrid(){
   
   for(let y = 0; y < GRIDSIZE; y++){
     for(let x = 0; x < GRIDSIZE; x++) {
-      // if(grid[y][x]===0){
-      //   fill("grey");
-      // }
-      // else{
-      //   fill("grey");
-      // }
+      
       fill("white");
       rect(cellWidth*x, cellHeight*y, cellWidth, cellHeight);
     }
@@ -48,22 +37,7 @@ function displayGrid(){
   }
 }
 
-// function generateRandomGrid(gridSize) {
-//   let grid = [];
-//   for (let i = 0; i< gridSize; i++){
-//     grid.push([]);
-//     for (let j = 0; j< gridSize; j++){
-//       if (random(100) < 50){
-//         grid[i].push(0);
-//       }
-//       else{
-//         grid[i].push(1);
-//       }
-  
-//     }
-//   }
-//   return grid;
-// }
+
 
 function generateEmptyGrid(GRIDSIZE) {
   let grid = [];
@@ -79,12 +53,4 @@ function generateEmptyGrid(GRIDSIZE) {
 function boxMove(){
   fill("black");
   rect(0, 0, cellWidth, cellHeight);
-  //let box = generateEmptyGrid(GRIDSIZE);
-  // for(let y = 0; y < GRIDSIZE; y++){
-  //   for(let x = 0; x < GRIDSIZE; x++){
-  // fill("black");
-  // rect(0, 0, cellWidth, cellHeight);
-
-  //   }
-  // }
-}
+  
